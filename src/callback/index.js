6035d299da4facad500b9145ff62b430d24dc5ce
jimmy Oliva <jsoliva@cemcol.com>
2024-06-12 16:46:24 -0600

    feat (asincronismo): callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,17 @@
+function sum(num1, num2){
+    return num1+num2;
+}
+
+function calc(num1, num2, callback){
+    return callback(num1, num2)
+}
+
+console.log(calc(2,2,sum))
+
+
+
+setTimeout(function (){
+    console.log("Hola JS")
+}, 3000)
+
+ 
